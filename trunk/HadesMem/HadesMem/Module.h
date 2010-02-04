41841f84ad52ccb1b9f5c5af1d516025755d9157
cypher.jb <cypher.jb@cdac023c-1092-11df-8e59-a5c54279f3f5>
2010-02-04 07:24:43 +0000

    * Temporary documentation for Module

--- trunk/HadesMem/HadesMem/Module.h
+++ trunk/HadesMem/HadesMem/Module.h
@@ -23,43 +23,103 @@ namespace Hades
 {
   namespace Memory
   {
+    // Module exception type
     class ModuleError : public virtual HadesMemError
     { };
 
+    // Get modules list
     inline std::vector<std::shared_ptr<class Module>> GetModuleList(
       MemoryMgr const& MyMemory);
 
+    // Module wide stream overload
     inline std::wostream& operator<< (std::wostream& Out, 
       class Module const& In);
 
+    // Module managing class
     class Module
     {
     public:
+      // Find module by name
       Module(std::wstring const& ModuleName, MemoryMgr const& MyMemory);
+      // Find module by handle
       Module(HMODULE Handle, MemoryMgr const& MyMemory);
 
+      // Get module base
       HMODULE GetBase() const;
+      // Get module size
       DWORD GetSize() const;
 
+      // Get module name
       std::wstring GetName() const;
+      // Get module path
       std::wstring GetPath() const;
 
+      // Whether module was found
       bool Found() const;
 
     private:
       // Disable assignment
       Module& operator= (Module const&);
 
+      // Memory instance
       MemoryMgr const& m_Memory;
 
+      // Module base address
       HMODULE m_Base;
+      // Module size
       DWORD m_Size;
+      // Module name
       std::wstring m_Name;
+      // Module path
       std::wstring m_Path;
 
+      // Whether module was found
       bool m_Found;
     };
 
+    // Get module list
+    inline std::vector<std::shared_ptr<class Module>> GetModuleList(
+      MemoryMgr const& MyMemory) 
+    {
+      // Grab a new snapshot of the process
+      EnsureCloseHandle const Snap(CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, 
+        MyMemory.GetProcessID()));
+      if (Snap == INVALID_HANDLE_VALUE)
+      {
+        DWORD LastError = GetLastError();
+        BOOST_THROW_EXCEPTION(ModuleError() << 
+          ErrorFunction("GetModuleList") << 
+          ErrorString("Could not get module snapshot.") << 
+          ErrorCodeWin(LastError));
+      }
+
+      // Container to hold module list
+      std::vector<std::shared_ptr<Module>> ModList;
+
+      // Search for process
+      MODULEENTRY32 ModEntry = { sizeof(ModEntry) };
+      for (BOOL MoreMods = Module32First(Snap, &ModEntry); MoreMods; 
+        MoreMods = Module32Next(Snap, &ModEntry)) 
+      {
+        ModList.push_back(std::make_shared<Module>(ModEntry.hModule, MyMemory));
+      }
+
+      // Return module list
+      return ModList;
+    }
+
+    // Module wide stream overload
+    inline std::wostream& operator<< (std::wostream& Stream, 
+      Module const& MyModule)
+    {
+      Stream << "Module Base: " << MyModule.GetBase() << "." << std::endl;
+      Stream << "Module Size: " << MyModule.GetSize() << "." << std::endl;
+      Stream << "Module Name: " << MyModule.GetName() << "." << std::endl;
+      Stream << "Module Path: " << MyModule.GetPath() << "." << std::endl;
+      return Stream;
+    }
+
+    // Find module by name
     Module::Module(std::wstring const& ModuleName, MemoryMgr const& MyMemory) 
       : m_Memory(MyMemory), 
       m_Base(nullptr), 
@@ -107,46 +167,7 @@ namespace Hades
       m_Found = Found;
     }
 
-    inline std::vector<std::shared_ptr<class Module>> GetModuleList(
-      MemoryMgr const& MyMemory) 
-    {
-      // Grab a new snapshot of the process
-      EnsureCloseHandle const Snap(CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, 
-        MyMemory.GetProcessID()));
-      if (Snap == INVALID_HANDLE_VALUE)
-      {
-        DWORD LastError = GetLastError();
-        BOOST_THROW_EXCEPTION(ModuleError() << 
-          ErrorFunction("GetModuleList") << 
-          ErrorString("Could not get module snapshot.") << 
-          ErrorCodeWin(LastError));
-      }
-
-      // Container to hold module list
-      std::vector<std::shared_ptr<Module>> ModList;
-
-      // Search for process
-      MODULEENTRY32 ModEntry = { sizeof(ModEntry) };
-      for (BOOL MoreMods = Module32First(Snap, &ModEntry); MoreMods; 
-        MoreMods = Module32Next(Snap, &ModEntry)) 
-      {
-        ModList.push_back(std::make_shared<Module>(ModEntry.hModule, MyMemory));
-      }
-
-      // Return module list
-      return ModList;
-    }
-
-    inline std::wostream& operator<< (std::wostream& Stream, 
-      Module const& MyModule)
-    {
-      Stream << "Module Base: " << MyModule.GetBase() << "." << std::endl;
-      Stream << "Module Size: " << MyModule.GetSize() << "." << std::endl;
-      Stream << "Module Name: " << MyModule.GetName() << "." << std::endl;
-      Stream << "Module Path: " << MyModule.GetPath() << "." << std::endl;
-      return Stream;
-    }
-
+    // Find module by handle
     Module::Module(HMODULE Handle, MemoryMgr const& MyMemory) 
       : m_Memory(MyMemory), 
       m_Base(nullptr), 
@@ -195,26 +216,31 @@ namespace Hades
       m_Path = ModEntry.szExePath;
     }
 
+    // Get module base address
     HMODULE Module::GetBase() const
     {
       return m_Base;
     }
 
+    // Get module size
     DWORD Module::GetSize() const
     {
       return m_Size;
     }
 
+    // Get module name
     std::wstring Module::GetName() const
     {
       return m_Name;
     }
 
+    // Get module path
     std::wstring Module::GetPath() const
     {
       return m_Path;
     }
 
+    // Whether module was found
     bool Module::Found() const
     {
       return m_Found;
