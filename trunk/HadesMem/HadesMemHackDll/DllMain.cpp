47517f1196087a17da7ca981880e9a092cbebe6c
cypher.jb <cypher.jb@cdac023c-1092-11df-8e59-a5c54279f3f5>
2010-03-02 02:55:40 +0000

    * HadesMemHackDLL: TLS testing code added.

--- trunk/HadesMem/HadesMemHackDll/DllMain.cpp
+++ trunk/HadesMem/HadesMemHackDll/DllMain.cpp
@@ -4,9 +4,21 @@
 // C++ Standard Library
 #include <cstdio>
 #include <vector>
+#include <string>
+
+// Boost
+#pragma warning(push, 1)
+#include <boost/thread.hpp>
+#pragma warning(pop)
 
 extern "C" __declspec(dllexport) void Initialize(HMODULE /*MyModule*/)
-{ }
+{
+  // Force TLS to be used
+  boost::thread_specific_ptr<std::string> MyInt;
+  if (!MyInt.get())
+    MyInt.reset();
+  *MyInt = "asdf";
+}
 
 BOOL WINAPI DllMain(HINSTANCE /*hinstDLL*/, DWORD /*fdwReason*/, 
   LPVOID /*lpvReserved*/)
