9286ed1f9e2d8a0830b493e4046fd477f953d8bb
cypher.jb <cypher.jb@cdac023c-1092-11df-8e59-a5c54279f3f5>
2010-03-21 19:49:39 +0000

    * More in-your-face copyright and licensing notices.

--- trunk/HadesMem/HadesMem/I18n.h
+++ trunk/HadesMem/HadesMem/I18n.h
@@ -1,5 +1,7 @@
 /*
 This file is part of HadesMem.
+Copyright © 2010 Cypherjb (aka Chazwazza, aka Cypher). 
+<http://www.cypherjb.com/> <cypher.jb@gmail.com>
 
 HadesMem is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
