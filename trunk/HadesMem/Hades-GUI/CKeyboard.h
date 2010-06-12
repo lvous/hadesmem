5f7d58336a800f736208da537028674bb88616c5
cypher.jb <cypher.jb@cdac023c-1092-11df-8e59-a5c54279f3f5>
2010-06-12 04:36:35 +0000

    * Hades-GUI: AMD64 fix.

--- trunk/HadesMem/Hades-GUI/CKeyboard.h
+++ trunk/HadesMem/Hades-GUI/CKeyboard.h
@@ -56,7 +56,7 @@ class CKeyboard
 
 public:
 
-	bool HandleMessage( unsigned int uMsg, __w64 unsigned int wParam, _w64 long lParam );
+	bool HandleMessage( unsigned int uMsg, WPARAM wParam, LPARAM lParam );
 
 	void SetKey( SKey sKey );
 	SKey GetKey();
