7821f077472785faceb4299487fdab635144b2f9
cypher.jb <cypher.jb@cdac023c-1092-11df-8e59-a5c54279f3f5>
2010-06-20 06:55:11 +0000

    * Hades-Common: Fixed string conversion code and removed some unused APIs which I have replaced with Boost alternatives.

--- trunk/HadesMem/Hades-Common/I18n.h
+++ trunk/HadesMem/Hades-Common/I18n.h
@@ -23,50 +23,11 @@ along with HadesMem.  If not, see <http://www.gnu.org/licenses/>.
 #include <string>
 #include <cctype>
 #include <locale>
+#include <iterator>
 #include <algorithm>
 
 // Boost
 #include <boost/lexical_cast.hpp>
-
-// Boost.LexicalCast specialization to allow conversions from wide to narrow 
-// strings. Thanks Xeno123 for the idea.
-template<> 
-inline std::string boost::lexical_cast<std::string, std::wstring>(
-  std::wstring const& Source)
-{
-//   auto const Loc(std::locale(""));
-//   auto const& MyCType(std::use_facet<std::ctype<wchar_t>>(Loc));
-// 
-//   std::string Dest;
-//   std::for_each(Source.begin(), Source.end(), 
-//     [&Dest, &MyCType] (wchar_t Current)
-//   {
-//     Dest += MyCType.narrow(Current);
-//   });
-// 
-//   return Dest;
-  return std::string(Source.begin(), Source.end());
-}
-
-// Boost.LexicalCast specialization to allow conversions from narrow to wide 
-// strings. Thanks Xeno123 for the idea.
-template<> 
-inline std::wstring boost::lexical_cast<std::wstring, std::string>(
-  std::string const& Source)
-{
-//   auto const Loc(std::locale(""));
-//   auto const& MyCType(std::use_facet<std::ctype<char>>(Loc));
-// 
-//   std::wstring Dest;
-//   std::for_each(Source.begin(), Source.end(), 
-//     [&Dest, &MyCType] (char Current)
-//   {
-//     Dest += MyCType.widen(Current);
-//   });
-// 
-//   return Dest;
-  return std::wstring(Source.begin(), Source.end());
-}
 
 namespace Hades
 {
@@ -79,10 +40,10 @@ namespace Hades
       auto const& MyCType(std::use_facet<std::ctype<wchar_t>>(Loc));
 
       std::string Dest;
-      std::for_each(Source.begin(), Source.end(), 
-        [&Dest, &MyCType] (wchar_t Current)
+      std::transform(Source.begin(), Source.end(), std::back_inserter(Dest), 
+        [&] (wchar_t Current)
       {
-        Dest += MyCType.narrow(Current);
+        return MyCType.narrow(Current);
       });
 
       return Dest;
@@ -95,47 +56,31 @@ namespace Hades
       auto const& MyCType(std::use_facet<std::ctype<char>>(Loc));
 
       std::wstring Dest;
-      std::for_each(Source.begin(), Source.end(), 
-        [&Dest, &MyCType] (char Current)
+      std::transform(Source.begin(), Source.end(), std::back_inserter(Dest), 
+        [&] (char Current)
       {
-        Dest += MyCType.widen(Current);
-      });
-
-      return Dest;
-    }
-
-    // Convert string to lowercase
-    template <typename CharT>
-    std::basic_string<CharT> ToLower(std::basic_string<CharT> const& Str)
-    {
-      auto const Loc(std::locale(""));
-      auto const& MyCType(std::use_facet<std::ctype<CharT>>(Loc));
-
-      std::basic_string<CharT> Dest(Str);
-      std::transform(Dest.begin(), Dest.end(), Dest.begin(), 
-        [&MyCType] (CharT x) 
-      {
-        return MyCType.tolower(x);
-      });
-
-      return Dest;
-    }
-
-    // Convert string to lowercase
-    template <typename CharT>
-    std::basic_string<CharT> ToUpper(std::basic_string<CharT> const& Str)
-    {
-      auto const Loc(std::locale(""));
-      auto const& MyCType(std::use_facet<std::ctype<CharT>>(Loc));
-
-      std::basic_string<CharT> Dest(Str);
-      std::transform(Dest.begin(), Dest.end(), Dest.begin(), 
-        [&MyCType] (CharT x) 
-      {
-        return MyCType.toupper(x);
+        return MyCType.widen(Current);
       });
 
       return Dest;
     }
   }
+}
+
+// Boost.LexicalCast specialization to allow conversions from wide to narrow 
+// strings. Thanks Xeno123 for the idea.
+template<> 
+inline std::string boost::lexical_cast<std::string, std::wstring>(
+  std::wstring const& Source)
+{
+  return Hades::Util::ConvertStr(Source);
+}
+
+// Boost.LexicalCast specialization to allow conversions from narrow to wide 
+// strings. Thanks Xeno123 for the idea.
+template<> 
+inline std::wstring boost::lexical_cast<std::wstring, std::string>(
+  std::string const& Source)
+{
+  return Hades::Util::ConvertStr(Source);
 }
