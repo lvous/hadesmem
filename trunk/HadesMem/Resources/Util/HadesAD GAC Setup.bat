ad50d59620cfb9c653434922c517ca1bafb3754c
cypher.jb <cypher.jb@cdac023c-1092-11df-8e59-a5c54279f3f5>
2010-07-08 00:30:16 +0000

    * Readded GAC setup script.

---
+++ trunk/HadesMem/Resources/Util/HadesAD GAC Setup.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools\gacutil.exe" /i "../HadesAD.dll"
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools\gacutil.exe" /l "HadesAD" > "GAC-Names.txt"
+pause
