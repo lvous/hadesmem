adee09ec1d78db1d24b5843a97753ad578f56386
cypher.jb <cypher.jb@cdac023c-1092-11df-8e59-a5c54279f3f5>
2010-07-08 02:59:28 +0000

    * Updated GAC registration script to use Windows SDK v7.1

--- trunk/HadesMem/Resources/Util/HadesAD GAC Setup.bat
+++ trunk/HadesMem/Resources/Util/HadesAD GAC Setup.bat
@@ -1,3 +1,3 @@
-"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools\gacutil.exe" /i "../HadesAD.dll"
-"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools\gacutil.exe" /l "HadesAD" > "GAC-Names.txt"
+"C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\NETFX 4.0 Tools\gacutil.exe" /i "../HadesAD.dll"
+"C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\NETFX 4.0 Tools\gacutil.exe" /l "HadesAD" > "GAC-Names.txt"
 pause
