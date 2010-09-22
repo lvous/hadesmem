4290cc3c18983ff75a0607f2544543890d69f63e
cypherjb <cypherjb@cdac023c-1092-11df-8e59-a5c54279f3f5>
2010-09-22 12:40:09 +0000

    * Restored implementation using Dinkumware's STL extensions. Speed increase was only in debug mode, I only care about speed in release mode, and this implementation is cleaner and easier to maintain (not to mention the underlying impl is probably a bit better than mine).

--- trunk/HadesMem-v2/Hades-Common/Hades-Common/I18n.h
+++ trunk/HadesMem-v2/Hades-Common/Hades-Common/I18n.h
@@ -21,28 +21,10 @@ along with HadesMem.  If not, see <http://www.gnu.org/licenses/>.
 
 // C++ Standard Library
 #include <string>
-#include <vector>
+#include <cvt/wstring>
 
 // Boost
-#pragma warning(push, 1)
-#pragma warning (disable: ALL_CODE_ANALYSIS_WARNINGS)
 #include <boost/lexical_cast.hpp>
-#pragma warning(pop)
-
-// Windows API
-#include <Windows.h>
-
-// Hades
-#include "Error.h"
-
-namespace Hades
-{
-  namespace I18n
-  {
-    class Error : public virtual HadesError
-    { };
-  }
-}
 
 // Boost.LexicalCast specialization to allow conversions from wide to narrow 
 // strings.
@@ -50,30 +32,8 @@ template<>
 inline std::string boost::lexical_cast<std::string, std::wstring>(
   std::wstring const& Source)
 {
-  int SrcLen = static_cast<int>(Source.length()) + 1;
-  int DestLen = WideCharToMultiByte(CP_ACP, 0, Source.c_str(), SrcLen, NULL, 
-    0, NULL, NULL);
-  if (!DestLen)
-  {
-    DWORD const LastError = GetLastError();
-    BOOST_THROW_EXCEPTION(Hades::I18n::Error() << 
-      Hades::ErrorFunction("boost::lexical_cast<std::string, std::wstring>") << 
-      Hades::ErrorString("Could not calculate target length.") << 
-      Hades::ErrorCodeWin(LastError));
-  }
-
-  std::vector<char> Dest(DestLen);
-  if (!WideCharToMultiByte(CP_ACP, 0, Source.c_str(), SrcLen, &Dest[0], 
-    DestLen, NULL, NULL))
-  {
-    DWORD const LastError = GetLastError();
-    BOOST_THROW_EXCEPTION(Hades::I18n::Error() << 
-      Hades::ErrorFunction("boost::lexical_cast<std::string, std::wstring>") << 
-      Hades::ErrorString("Could not convert string.") << 
-      Hades::ErrorCodeWin(LastError));
-  }
-
-  return &Dest[0];
+  return stdext::cvt::wstring_convert<std::codecvt<wchar_t, char, 
+    mbstate_t>>().to_bytes(Source);
 }
 
 // Boost.LexicalCast specialization to allow conversions from narrow to wide 
@@ -82,28 +42,6 @@ template<>
 inline std::wstring boost::lexical_cast<std::wstring, std::string>(
   std::string const& Source)
 {
-  int SrcLen = static_cast<int>(Source.length()) + 1;
-  int DestLen = MultiByteToWideChar(CP_ACP, 0, Source.c_str(), SrcLen, NULL, 
-    0);
-  if (!DestLen)
-  {
-    DWORD const LastError = GetLastError();
-    BOOST_THROW_EXCEPTION(Hades::I18n::Error() << 
-      Hades::ErrorFunction("boost::lexical_cast<std::wstring, std::string>") << 
-      Hades::ErrorString("Could not calculate target length.") << 
-      Hades::ErrorCodeWin(LastError));
-  }
-  
-  std::vector<wchar_t> Dest(DestLen);
-  if (!MultiByteToWideChar(CP_ACP, 0, Source.c_str(), SrcLen, &Dest[0], 
-    DestLen))
-  {
-    DWORD const LastError = GetLastError();
-    BOOST_THROW_EXCEPTION(Hades::I18n::Error() << 
-      Hades::ErrorFunction("boost::lexical_cast<std::wstring, std::string>") << 
-      Hades::ErrorString("Could not convert string.") << 
-      Hades::ErrorCodeWin(LastError));
-  }
-  
-  return &Dest[0];
+  return stdext::cvt::wstring_convert<std::codecvt<wchar_t, char, 
+    mbstate_t>>().from_bytes(Source);
 }
