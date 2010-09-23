91df31f5aaf0e7514c5ee1f78a3f1e9e86fb1096
cypherjb <cypherjb@cdac023c-1092-11df-8e59-a5c54279f3f5>
2010-09-23 15:59:00 +0000

    * Added manual map tester script. * All scripts are now architecture independent.

---
+++ trunk/HadesMem-v2/Hades-Memory/Scripts/WinDbgManMap.lua
@@ -0,0 +1,16 @@
+HadesMem.WriteLn("Opening WinDbg.");
+MyMem = HadesMem.MemoryMgr("windbg.exe");
+
+if HadesMem.IsAMD64() then
+	HadesMem.WriteLn("");
+	HadesMem.WriteLn("Manually mapping Hades-MMHelper_AMD64");
+	MyManMap = HadesMem.ManualMap(MyMem);
+	ManModBase = MyManMap:Map("../Hades-MMHelper_AMD64.dll", "Initialize", false);
+	HadesMem.WriteLn("Success! Module Base: " .. HadesMem.ToHexStr(ManModBase));	
+else
+	HadesMem.WriteLn("");
+	HadesMem.WriteLn("Manually mapping Hades-MMHelper_IA32");
+	MyManMap = HadesMem.ManualMap(MyMem);
+	ManModBase = MyManMap:Map("../Hades-MMHelper_IA32.dll", "_Initialize@4", false);
+	HadesMem.WriteLn("Success! Module Base: " .. HadesMem.ToHexStr(ManModBase));
+end
