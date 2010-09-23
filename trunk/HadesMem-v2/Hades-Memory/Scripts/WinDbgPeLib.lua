edbcddc03880ecf126c9f4dd01a8cf0aa9385a96
cypherjb <cypherjb@cdac023c-1092-11df-8e59-a5c54279f3f5>
2010-09-23 17:53:21 +0000

    * Improved script.

--- trunk/HadesMem-v2/Hades-Memory/Scripts/WinDbgPeLib.lua
+++ trunk/HadesMem-v2/Hades-Memory/Scripts/WinDbgPeLib.lua
@@ -1,38 +1,60 @@
 HadesMem.WriteLn("Opening WinDbg.");
 MyMem = HadesMem.MemoryMgr("windbg.exe");
 
-Kernel32Mod = HadesMem.Module(MyMem, "kernel32.dll");
-MyPeFile = HadesMem.PeFile(MyMem, Kernel32Mod:GetBase());
-MyDosHeader = HadesMem.DosHeader(MyPeFile);
-MyNtHeaders = HadesMem.NtHeaders(MyPeFile);
+MyModEnum = HadesMem.ModuleEnum(MyMem)
+MyMod = MyModEnum:First();
 
-HadesMem.WriteLn("");
-HadesMem.WriteLn("Dumping sections.");
-MySectionEnum = HadesMem.SectionEnum(MyPeFile);
-MySection = MySectionEnum:First();
-while MySection do
-	HadesMem.WriteLn(MySection:GetName());
-	MySection = MySectionEnum:Next();
-end
-
-MyExportDir = HadesMem.ExportDir(MyPeFile);
-
-HadesMem.WriteLn("");
-HadesMem.WriteLn("Kernel32 export module name: " .. MyExportDir:GetName());
-
-HadesMem.WriteLn("");
-HadesMem.WriteLn("Dumping exports.");
-MyExportEnum = HadesMem.ExportEnum(MyPeFile);
-MyExport = MyExportEnum:First();
-while MyExport do
-	if MyExport:ByName() then
-		if MyExport:Forwarded() then
-			HadesMem.WriteLn(MyExport:GetOrdinal() .. " -> " .. MyExport:GetName() .. " -> " .. MyExport:GetForwarder());
-		else
-			HadesMem.WriteLn(MyExport:GetOrdinal() .. " -> " .. MyExport:GetName());
+while MyMod do
+	MyPeFile = HadesMem.PeFile(MyMem, MyMod:GetBase());
+	MyDosHeader = HadesMem.DosHeader(MyPeFile);
+	MyNtHeaders = HadesMem.NtHeaders(MyPeFile);
+	
+	HadesMem.WriteLn("");
+	HadesMem.WriteLn("Dumping sections.");
+	MySectionEnum = HadesMem.SectionEnum(MyPeFile);
+	MySection = MySectionEnum:First();
+	while MySection do
+		HadesMem.WriteLn(MySection:GetName());
+		MySection = MySectionEnum:Next();
+	end
+	
+	MyTlsDir = HadesMem.TlsDir(MyPeFile);
+	if MyTlsDir:IsValid() then
+		HadesMem.WriteLn("");
+		HadesMem.WriteLn("Dumping TLS callbacks.");
+		TlsCallbacks = MyTlsDir:GetCallbacks();
+		for i in TlsCallbacks.List do
+			HadesMem.WriteLn(HadesMem.ToHexStr(i));
 		end
 	else
-		HadesMem.WriteLn(MyExport:GetOrdinal());
+		HadesMem.WriteLn("");
+		HadesMem.WriteLn("Image has no TLS directory.");
 	end
-	MyExport = MyExportEnum:Next();
+	
+	MyExportDir = HadesMem.ExportDir(MyPeFile);
+	if MyExportDir:IsValid() then
+		HadesMem.WriteLn("");
+		HadesMem.WriteLn("Export dir module name: " .. MyExportDir:GetName());
+		
+		HadesMem.WriteLn("");
+		HadesMem.WriteLn("Dumping exports.");
+		MyExportEnum = HadesMem.ExportEnum(MyPeFile);
+		MyExport = MyExportEnum:First();
+		while MyExport do
+			if MyExport:ByName() then
+				if MyExport:Forwarded() then
+					HadesMem.WriteLn(MyExport:GetOrdinal() .. " -> " .. MyExport:GetName() .. " -> " .. MyExport:GetForwarder());
+				else
+					HadesMem.WriteLn(MyExport:GetOrdinal() .. " -> " .. MyExport:GetName());
+				end
+			else
+				HadesMem.WriteLn("" .. MyExport:GetOrdinal());
+			end
+			MyExport = MyExportEnum:Next();
+		end
+	else
+		HadesMem.WriteLn("");
+		HadesMem.WriteLn("Image has no export directory.");
+	end
+	MyMod = MyModEnum:Next();
 end
