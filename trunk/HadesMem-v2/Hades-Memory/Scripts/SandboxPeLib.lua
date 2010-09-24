e3dc7913412098a2d11217c6a344a357f5fdc254
cypherjb <cypherjb@cdac023c-1092-11df-8e59-a5c54279f3f5>
2010-09-24 01:24:57 +0000

    * New test script. Uses itself as the target.

---
+++ trunk/HadesMem-v2/Hades-Memory/Scripts/SandboxPeLib.lua
@@ -0,0 +1,69 @@
+if HadesMem.IsAMD64() then
+	HadesMem.WriteLn("Opening Hades-MemSandbox_AMD64.");
+	MyMem = HadesMem.MemoryMgr("Hades-MemSandbox_AMD64.exe");
+else
+	HadesMem.WriteLn("Opening Hades-MemSandbox_IA32.");
+	MyMem = HadesMem.MemoryMgr("Hades-MemSandbox_IA32.exe");
+end
+
+MyModEnum = HadesMem.ModuleEnum(MyMem)
+MyMod = MyModEnum:First();
+
+while MyMod do
+	MyPeFile = HadesMem.PeFile(MyMem, MyMod:GetBase());
+	MyDosHeader = HadesMem.DosHeader(MyPeFile);
+	MyNtHeaders = HadesMem.NtHeaders(MyPeFile);
+	
+	HadesMem.WriteLn("");
+	HadesMem.WriteLn("Dumping sections.");
+	MySectionEnum = HadesMem.SectionEnum(MyPeFile);
+	MySection = MySectionEnum:First();
+	while MySection do
+		HadesMem.WriteLn(MySection:GetName());
+		MySection = MySectionEnum:Next();
+	end
+	
+	MyTlsDir = HadesMem.TlsDir(MyPeFile);
+	if MyTlsDir:IsValid() then
+		HadesMem.WriteLn("");
+		HadesMem.WriteLn("Dumping TLS callbacks.");
+		TlsCallbacks = MyTlsDir:GetCallbacks();
+		for i in TlsCallbacks.List do
+			HadesMem.WriteLn(HadesMem.ToHexStr(i));
+		end
+	else
+		HadesMem.WriteLn("");
+		HadesMem.WriteLn("Image has no TLS directory.");
+	end
+	
+	MyExportDir = HadesMem.ExportDir(MyPeFile);
+	if MyExportDir:IsValid() then
+		HadesMem.WriteLn("");
+		HadesMem.WriteLn("Export dir module name: " .. MyExportDir:GetName());
+		
+		HadesMem.WriteLn("");
+		HadesMem.WriteLn("Dumping exports.");
+		MyExportEnum = HadesMem.ExportEnum(MyPeFile);
+		MyExport = MyExportEnum:First();
+		while MyExport do
+			if MyExport:ByName() then
+				if MyExport:Forwarded() then
+					HadesMem.WriteLn(MyExport:GetOrdinal() .. " -> " .. MyExport:GetName() .. " -> " .. MyExport:GetForwarder());
+				else
+					HadesMem.WriteLn(MyExport:GetOrdinal() .. " -> " .. MyExport:GetName());
+				end
+			else
+				if MyExport:Forwarded() then
+					HadesMem.WriteLn(MyExport:GetOrdinal() .. " -> " .. MyExport:GetForwarder());
+				else
+					HadesMem.WriteLn(MyExport:GetOrdinal() .. "");
+				end
+			end
+			MyExport = MyExportEnum:Next();
+		end
+	else
+		HadesMem.WriteLn("");
+		HadesMem.WriteLn("Image has no export directory.");
+	end
+	MyMod = MyModEnum:Next();
+end
