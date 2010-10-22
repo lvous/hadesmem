d1db7fc62ea49d16877530c76a6eefd6f671091d
cypherjb <cypherjb@cdac023c-1092-11df-8e59-a5c54279f3f5>
2010-10-22 15:57:57 +0000

    * Started adding Python support to replace Lua due to Lua's lack of 64-bit number support (See: http://www.raptorfactor.com/lualuabind-64-bit-number-support/). This is going to be a long and slow transition, so please be patient. Help would be greatly appreciated though, so please contact me if you have the time.

---
+++ trunk/HadesMem-v2/Hades-Memory/PyHadesMem/Main.cpp
@@ -0,0 +1,59 @@
+/*
+This file is part of HadesMem.
+Copyright © 2010 RaptorFactor (aka Cypherjb, Cypher, Chazwazza). 
+<http://www.raptorfactor.com/> <raptorfactor@raptorfactor.com>
+
+HadesMem is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+HadesMem is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with HadesMem.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Windows API
+#include <crtdbg.h>
+#include <Windows.h>
+
+// Boost
+#pragma warning(push, 1)
+#pragma warning (disable: ALL_CODE_ANALYSIS_WARNINGS)
+#include <boost/python.hpp>
+#pragma warning(pop)
+
+char const* greet()
+{
+  return "hello, world";
+}
+
+#if defined(_M_AMD64) 
+BOOST_PYTHON_MODULE(PyHadesMem_AMD64)
+#elif defined(_M_IX86) 
+BOOST_PYTHON_MODULE(PyHadesMem_IA32)
+#else 
+#error "Unsupported architecture."
+#endif
+{
+  using namespace boost::python;
+  def("greet", greet);
+}
+
+BOOL WINAPI DllMain(HINSTANCE /*hinstDLL*/, DWORD /*fdwReason*/, 
+  LPVOID /*lpvReserved*/)
+{
+  // Attempt to detect memory leaks in debug mode
+#ifdef _DEBUG
+  int CurrentFlags = _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG);
+  int NewFlags = (_CRTDBG_DELAY_FREE_MEM_DF | _CRTDBG_LEAK_CHECK_DF | 
+    _CRTDBG_CHECK_ALWAYS_DF);
+  _CrtSetDbgFlag(CurrentFlags | NewFlags);
+#endif
+
+  return TRUE;
+}
