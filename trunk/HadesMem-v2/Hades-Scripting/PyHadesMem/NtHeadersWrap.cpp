08d74ff5a7e5b0cc82617f23239322cd0fee5b3c
cypherjb <cypherjb@cdac023c-1092-11df-8e59-a5c54279f3f5>
2010-12-12 10:20:40 +0000

    * Formatting.

--- trunk/HadesMem-v2/Hades-Scripting/PyHadesMem/NtHeadersWrap.cpp
+++ trunk/HadesMem-v2/Hades-Scripting/PyHadesMem/NtHeadersWrap.cpp
@@ -26,141 +26,123 @@ along with HadesMem.  If not, see <http://www.gnu.org/licenses/>.
 // Hades
 #include "Hades-Memory/NtHeaders.h"
 
+class NtHeadersWrap : public Hades::Memory::NtHeaders
+{
+public:
+  NtHeadersWrap(Hades::Memory::PeFile const& MyPeFile)
+    : Hades::Memory::NtHeaders(MyPeFile)
+  { }
+
+  DWORD_PTR GetBase() const
+  {
+    return reinterpret_cast<DWORD_PTR>(Hades::Memory::NtHeaders::GetBase());
+  }
+};
+
 // Export NtHeaders API
 void ExportNtHeaders()
 {
-  boost::python::scope NtHeadersScope = boost::python::class_<
-    Hades::Memory::NtHeaders>("NtHeaders", boost::python::init<
-    Hades::Memory::PeFile const&>())
-//     .def("GetBase", &Hades::Memory::NtHeaders::GetBase)
-    .def("IsSignatureValid", &Hades::Memory::NtHeaders::IsSignatureValid)
-    .def("EnsureSignatureValid", &Hades::Memory::NtHeaders::
-      EnsureSignatureValid)
-    .def("GetSignature", &Hades::Memory::NtHeaders::GetSignature)
-    .def("SetSignature", &Hades::Memory::NtHeaders::SetSignature)
-    .def("GetMachine", &Hades::Memory::NtHeaders::GetMachine)
-    .def("SetMachine", &Hades::Memory::NtHeaders::SetMachine)
-    .def("GetNumberOfSections", &Hades::Memory::NtHeaders::GetNumberOfSections)
-    .def("SetNumberOfSections", &Hades::Memory::NtHeaders::SetNumberOfSections)
-    .def("GetTimeDateStamp", &Hades::Memory::NtHeaders::GetTimeDateStamp)
-    .def("SetTimeDateStamp", &Hades::Memory::NtHeaders::SetTimeDateStamp)
-    .def("GetPointerToSymbolTable", &Hades::Memory::NtHeaders::
-      GetPointerToSymbolTable)
-    .def("SetPointerToSymbolTable", &Hades::Memory::NtHeaders::
-      SetPointerToSymbolTable)
-    .def("GetNumberOfSymbols", &Hades::Memory::NtHeaders::GetNumberOfSymbols)
-    .def("SetNumberOfSymbols", &Hades::Memory::NtHeaders::SetNumberOfSymbols)
-    .def("GetSizeOfOptionalHeader", &Hades::Memory::NtHeaders::
-      GetSizeOfOptionalHeader)
-    .def("SetSizeOfOptionalHeader", &Hades::Memory::NtHeaders::
-      SetSizeOfOptionalHeader)
-    .def("GetCharacteristics", &Hades::Memory::NtHeaders::GetCharacteristics)
-    .def("SetCharacteristics", &Hades::Memory::NtHeaders::SetCharacteristics)
-    .def("GetMagic", &Hades::Memory::NtHeaders::GetMagic)
-    .def("SetMagic", &Hades::Memory::NtHeaders::SetMagic)
-    .def("GetMajorLinkerVersion", &Hades::Memory::NtHeaders::
-      GetMajorLinkerVersion)
-    .def("SetMajorLinkerVersion", &Hades::Memory::NtHeaders::
-      SetMajorLinkerVersion)
-    .def("GetMinorLinkerVersion", &Hades::Memory::NtHeaders::
-      GetMinorLinkerVersion)
-    .def("SetMinorLinkerVersion", &Hades::Memory::NtHeaders::
-      SetMinorLinkerVersion)
-    .def("GetSizeOfCode", &Hades::Memory::NtHeaders::GetSizeOfCode)
-    .def("SetSizeOfCode", &Hades::Memory::NtHeaders::SetSizeOfCode)
-    .def("GetSizeOfInitializedData", &Hades::Memory::NtHeaders::
-      GetSizeOfInitializedData)
-    .def("SetSizeOfInitializedData", &Hades::Memory::NtHeaders::
-      SetSizeOfInitializedData)
-    .def("GetSizeOfUninitializedData", &Hades::Memory::NtHeaders::
-      GetSizeOfUninitializedData)
-    .def("SetSizeOfUninitializedData", &Hades::Memory::NtHeaders::
-      SetSizeOfUninitializedData)
-    .def("GetAddressOfEntryPoint", &Hades::Memory::NtHeaders::
-      GetAddressOfEntryPoint)
-    .def("SetAddressOfEntryPoint", &Hades::Memory::NtHeaders::
-      SetAddressOfEntryPoint)
-    .def("GetBaseOfCode", &Hades::Memory::NtHeaders::GetBaseOfCode)
-    .def("SetBaseOfCode", &Hades::Memory::NtHeaders::SetBaseOfCode)
+  boost::python::class_<Hades::Memory::NtHeaders>("NtHeadersBase", 
+    boost::python::no_init)
+    ;
+
+  boost::python::scope NtHeadersScope = boost::python::class_<NtHeadersWrap, 
+    boost::python::bases<Hades::Memory::NtHeaders>>("NtHeaders", 
+    boost::python::init<Hades::Memory::PeFile const&>())
+    .def("GetBase", &NtHeadersWrap::GetBase)
+    .def("IsSignatureValid", &NtHeadersWrap::IsSignatureValid)
+    .def("EnsureSignatureValid", &NtHeadersWrap::EnsureSignatureValid)
+    .def("GetSignature", &NtHeadersWrap::GetSignature)
+    .def("SetSignature", &NtHeadersWrap::SetSignature)
+    .def("GetMachine", &NtHeadersWrap::GetMachine)
+    .def("SetMachine", &NtHeadersWrap::SetMachine)
+    .def("GetNumberOfSections", &NtHeadersWrap::GetNumberOfSections)
+    .def("SetNumberOfSections", &NtHeadersWrap::SetNumberOfSections)
+    .def("GetTimeDateStamp", &NtHeadersWrap::GetTimeDateStamp)
+    .def("SetTimeDateStamp", &NtHeadersWrap::SetTimeDateStamp)
+    .def("GetPointerToSymbolTable", &NtHeadersWrap::GetPointerToSymbolTable)
+    .def("SetPointerToSymbolTable", &NtHeadersWrap::SetPointerToSymbolTable)
+    .def("GetNumberOfSymbols", &NtHeadersWrap::GetNumberOfSymbols)
+    .def("SetNumberOfSymbols", &NtHeadersWrap::SetNumberOfSymbols)
+    .def("GetSizeOfOptionalHeader", &NtHeadersWrap::GetSizeOfOptionalHeader)
+    .def("SetSizeOfOptionalHeader", &NtHeadersWrap::SetSizeOfOptionalHeader)
+    .def("GetCharacteristics", &NtHeadersWrap::GetCharacteristics)
+    .def("SetCharacteristics", &NtHeadersWrap::SetCharacteristics)
+    .def("GetMagic", &NtHeadersWrap::GetMagic)
+    .def("SetMagic", &NtHeadersWrap::SetMagic)
+    .def("GetMajorLinkerVersion", &NtHeadersWrap::GetMajorLinkerVersion)
+    .def("SetMajorLinkerVersion", &NtHeadersWrap::SetMajorLinkerVersion)
+    .def("GetMinorLinkerVersion", &NtHeadersWrap::GetMinorLinkerVersion)
+    .def("SetMinorLinkerVersion", &NtHeadersWrap::SetMinorLinkerVersion)
+    .def("GetSizeOfCode", &NtHeadersWrap::GetSizeOfCode)
+    .def("SetSizeOfCode", &NtHeadersWrap::SetSizeOfCode)
+    .def("GetSizeOfInitializedData", &NtHeadersWrap::GetSizeOfInitializedData)
+    .def("SetSizeOfInitializedData", &NtHeadersWrap::SetSizeOfInitializedData)
+    .def("GetSizeOfUninitializedData", &NtHeadersWrap::
+    GetSizeOfUninitializedData)
+    .def("SetSizeOfUninitializedData", &NtHeadersWrap::
+    SetSizeOfUninitializedData)
+    .def("GetAddressOfEntryPoint", &NtHeadersWrap::GetAddressOfEntryPoint)
+    .def("SetAddressOfEntryPoint", &NtHeadersWrap::SetAddressOfEntryPoint)
+    .def("GetBaseOfCode", &NtHeadersWrap::GetBaseOfCode)
+    .def("SetBaseOfCode", &NtHeadersWrap::SetBaseOfCode)
 #if defined(_M_IX86) 
-    .def("GetBaseOfData", &Hades::Memory::NtHeaders::GetBaseOfData)
-    .def("SetBaseOfData", &Hades::Memory::NtHeaders::SetBaseOfData)
+    .def("GetBaseOfData", &NtHeadersWrap::GetBaseOfData)
+    .def("SetBaseOfData", &NtHeadersWrap::SetBaseOfData)
 #endif
-    .def("GetImageBase", &Hades::Memory::NtHeaders::GetImageBase)
-    .def("SetImageBase", &Hades::Memory::NtHeaders::SetImageBase)
-    .def("GetSectionAlignment", &Hades::Memory::NtHeaders::GetSectionAlignment)
-    .def("SetSectionAlignment", &Hades::Memory::NtHeaders::SetSectionAlignment)
-    .def("GetFileAlignment", &Hades::Memory::NtHeaders::GetFileAlignment)
-    .def("SetFileAlignment", &Hades::Memory::NtHeaders::SetFileAlignment)
-    .def("GetMajorOperatingSystemVersion", &Hades::Memory::NtHeaders::
-      GetMajorOperatingSystemVersion)
-    .def("SetMajorOperatingSystemVersion", &Hades::Memory::NtHeaders::
-      SetMajorOperatingSystemVersion)
-    .def("GetMinorOperatingSystemVersion", &Hades::Memory::NtHeaders::
-      GetMinorOperatingSystemVersion)
-    .def("SetMinorOperatingSystemVersion", &Hades::Memory::NtHeaders::
-      SetMinorOperatingSystemVersion)
-    .def("GetMajorImageVersion", &Hades::Memory::NtHeaders::
-      GetMajorImageVersion)
-    .def("SetMajorImageVersion", &Hades::Memory::NtHeaders::
-      SetMajorImageVersion)
-    .def("GetMinorImageVersion", &Hades::Memory::NtHeaders::
-      GetMinorImageVersion)
-    .def("SetMinorImageVersion", &Hades::Memory::NtHeaders::
-      SetMinorImageVersion)
-    .def("GetMajorSubsystemVersion", &Hades::Memory::NtHeaders::
-      GetMajorSubsystemVersion)
-    .def("SetMajorSubsystemVersion", &Hades::Memory::NtHeaders::
-      SetMajorSubsystemVersion)
-    .def("GetMinorSubsystemVersion", &Hades::Memory::NtHeaders::
-      GetMinorSubsystemVersion)
-    .def("SetMinorSubsystemVersion", &Hades::Memory::NtHeaders::
-      SetMinorSubsystemVersion)
-    .def("GetWin32VersionValue", &Hades::Memory::NtHeaders::
-      GetWin32VersionValue)
-    .def("SetWin32VersionValue", &Hades::Memory::NtHeaders::
-      SetWin32VersionValue)
-    .def("GetSizeOfImage", &Hades::Memory::NtHeaders::GetSizeOfImage)
-    .def("SetSizeOfImage", &Hades::Memory::NtHeaders::SetSizeOfImage)
-    .def("GetSizeOfHeaders", &Hades::Memory::NtHeaders::GetSizeOfHeaders)
-    .def("SetSizeOfHeaders", &Hades::Memory::NtHeaders::SetSizeOfHeaders)
-    .def("GetCheckSum", &Hades::Memory::NtHeaders::GetCheckSum)
-    .def("SetCheckSum", &Hades::Memory::NtHeaders::SetCheckSum)
-    .def("GetSubsystem", &Hades::Memory::NtHeaders::GetSubsystem)
-    .def("SetSubsystem", &Hades::Memory::NtHeaders::SetSubsystem)
-    .def("GetDllCharacteristics", &Hades::Memory::NtHeaders::
-      GetDllCharacteristics)
-    .def("SetDllCharacteristics", &Hades::Memory::NtHeaders::
-      SetDllCharacteristics)
-    .def("GetSizeOfStackReserve", &Hades::Memory::NtHeaders::
-      GetSizeOfStackReserve)
-    .def("SetSizeOfStackReserve", &Hades::Memory::NtHeaders::
-      SetSizeOfStackReserve)
-    .def("GetSizeOfStackCommit", &Hades::Memory::NtHeaders::
-      GetSizeOfStackCommit)
-    .def("SetSizeOfStackCommit", &Hades::Memory::NtHeaders::
-      SetSizeOfStackCommit)
-    .def("GetSizeOfHeapReserve", &Hades::Memory::NtHeaders::
-      GetSizeOfHeapReserve)
-    .def("SetSizeOfHeapReserve", &Hades::Memory::NtHeaders::
-      SetSizeOfHeapReserve)
-    .def("GetSizeOfHeapCommit", &Hades::Memory::NtHeaders::GetSizeOfHeapCommit)
-    .def("SetSizeOfHeapCommit", &Hades::Memory::NtHeaders::SetSizeOfHeapCommit)
-    .def("GetLoaderFlags", &Hades::Memory::NtHeaders::GetLoaderFlags)
-    .def("SetLoaderFlags", &Hades::Memory::NtHeaders::SetLoaderFlags)
-    .def("GetNumberOfRvaAndSizes", &Hades::Memory::NtHeaders::
-      GetNumberOfRvaAndSizes)
-    .def("SetNumberOfRvaAndSizes", &Hades::Memory::NtHeaders::
-      SetNumberOfRvaAndSizes)
-    .def("GetDataDirectoryVirtualAddress", &Hades::Memory::NtHeaders::
-      GetDataDirectoryVirtualAddress)
-    .def("SetDataDirectoryVirtualAddress", &Hades::Memory::NtHeaders::
-      SetDataDirectoryVirtualAddress)
-    .def("GetDataDirectorySize", &Hades::Memory::NtHeaders::
-      GetDataDirectorySize)
-    .def("SetDataDirectorySize", &Hades::Memory::NtHeaders::
-      SetDataDirectorySize)
-    .def("GetHeadersRaw", &Hades::Memory::NtHeaders::GetHeadersRaw)
+    .def("GetImageBase", &NtHeadersWrap::GetImageBase)
+    .def("SetImageBase", &NtHeadersWrap::SetImageBase)
+    .def("GetSectionAlignment", &NtHeadersWrap::GetSectionAlignment)
+    .def("SetSectionAlignment", &NtHeadersWrap::SetSectionAlignment)
+    .def("GetFileAlignment", &NtHeadersWrap::GetFileAlignment)
+    .def("SetFileAlignment", &NtHeadersWrap::SetFileAlignment)
+    .def("GetMajorOperatingSystemVersion", &NtHeadersWrap::
+    GetMajorOperatingSystemVersion)
+    .def("SetMajorOperatingSystemVersion", &NtHeadersWrap::
+    SetMajorOperatingSystemVersion)
+    .def("GetMinorOperatingSystemVersion", &NtHeadersWrap::
+    GetMinorOperatingSystemVersion)
+    .def("SetMinorOperatingSystemVersion", &NtHeadersWrap::
+    SetMinorOperatingSystemVersion)
+    .def("GetMajorImageVersion", &NtHeadersWrap::GetMajorImageVersion)
+    .def("SetMajorImageVersion", &NtHeadersWrap::SetMajorImageVersion)
+    .def("GetMinorImageVersion", &NtHeadersWrap::GetMinorImageVersion)
+    .def("SetMinorImageVersion", &NtHeadersWrap::SetMinorImageVersion)
+    .def("GetMajorSubsystemVersion", &NtHeadersWrap::GetMajorSubsystemVersion)
+    .def("SetMajorSubsystemVersion", &NtHeadersWrap::SetMajorSubsystemVersion)
+    .def("GetMinorSubsystemVersion", &NtHeadersWrap::GetMinorSubsystemVersion)
+    .def("SetMinorSubsystemVersion", &NtHeadersWrap::SetMinorSubsystemVersion)
+    .def("GetWin32VersionValue", &NtHeadersWrap::GetWin32VersionValue)
+    .def("SetWin32VersionValue", &NtHeadersWrap::SetWin32VersionValue)
+    .def("GetSizeOfImage", &NtHeadersWrap::GetSizeOfImage)
+    .def("SetSizeOfImage", &NtHeadersWrap::SetSizeOfImage)
+    .def("GetSizeOfHeaders", &NtHeadersWrap::GetSizeOfHeaders)
+    .def("SetSizeOfHeaders", &NtHeadersWrap::SetSizeOfHeaders)
+    .def("GetCheckSum", &NtHeadersWrap::GetCheckSum)
+    .def("SetCheckSum", &NtHeadersWrap::SetCheckSum)
+    .def("GetSubsystem", &NtHeadersWrap::GetSubsystem)
+    .def("SetSubsystem", &NtHeadersWrap::SetSubsystem)
+    .def("GetDllCharacteristics", &NtHeadersWrap::GetDllCharacteristics)
+    .def("SetDllCharacteristics", &NtHeadersWrap::SetDllCharacteristics)
+    .def("GetSizeOfStackReserve", &NtHeadersWrap::GetSizeOfStackReserve)
+    .def("SetSizeOfStackReserve", &NtHeadersWrap::SetSizeOfStackReserve)
+    .def("GetSizeOfStackCommit", &NtHeadersWrap::GetSizeOfStackCommit)
+    .def("SetSizeOfStackCommit", &NtHeadersWrap::SetSizeOfStackCommit)
+    .def("GetSizeOfHeapReserve", &NtHeadersWrap::GetSizeOfHeapReserve)
+    .def("SetSizeOfHeapReserve", &NtHeadersWrap::SetSizeOfHeapReserve)
+    .def("GetSizeOfHeapCommit", &NtHeadersWrap::GetSizeOfHeapCommit)
+    .def("SetSizeOfHeapCommit", &NtHeadersWrap::SetSizeOfHeapCommit)
+    .def("GetLoaderFlags", &NtHeadersWrap::GetLoaderFlags)
+    .def("SetLoaderFlags", &NtHeadersWrap::SetLoaderFlags)
+    .def("GetNumberOfRvaAndSizes", &NtHeadersWrap::GetNumberOfRvaAndSizes)
+    .def("SetNumberOfRvaAndSizes", &NtHeadersWrap::SetNumberOfRvaAndSizes)
+    .def("GetDataDirectoryVirtualAddress", &NtHeadersWrap::
+    GetDataDirectoryVirtualAddress)
+    .def("SetDataDirectoryVirtualAddress", &NtHeadersWrap::
+    SetDataDirectoryVirtualAddress)
+    .def("GetDataDirectorySize", &NtHeadersWrap::GetDataDirectorySize)
+    .def("SetDataDirectorySize", &NtHeadersWrap::SetDataDirectorySize)
+    .def("GetHeadersRaw", &NtHeadersWrap::GetHeadersRaw)
     ;
 
   boost::python::enum_<Hades::Memory::NtHeaders::DataDir>("DataDir")
